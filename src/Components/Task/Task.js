244074080d97c7f29bbb914964ad44c695664507
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-16 11:26:16 +0200

    Add fields
    Add necessary field attributes

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -4,11 +4,18 @@ import '../ContextMenu/ContextMenu';
 import ContextMenu from '../ContextMenu/ContextMenu';
 
 class Task extends React.Component {
+
+    id = "";
+    title = "";
+    status = false;
+    tasks = [];
+    duedate = "";
+
     render (){
-        return <div class="task-container">
+        return <div class="task-container" id={"task-container-".concat(this.props.level).concat(this.props.index)}>
             <span class="task-container-header">
                 <div class="task-container-title">Hey there</div>
-                <ContextMenu level={this.props.level}></ContextMenu>
+                <ContextMenu level={this.props.level} index={this.props.index}></ContextMenu>
             </span>
             <br></br>
             <div class="item-container" id={"item-container-level-".concat(this.props.level)} children="0"></div>
