b87ce8369df41e18721830992714fd65313bcf0e
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-16 12:13:27 +0200

    Added Backend Functionality
    Added method functionality and helper methods

--- src/Services/TaskHelper.js
+++ src/Services/TaskHelper.js
@@ -1,25 +1,54 @@
-import React from 'react';
+const homeUrl = "https://z8sp8dl9if.execute-api.eu-west-2.amazonaws.com/dev/";
+const userID = "2509";
 
-const addRootTask = () => {
- 
+const createURL = (id) => {
+    let urlFinal = homeUrl + "{" + userID + "}/tasks";
+    if (id === "-1") {
+        return urlFinal;
+    } else {
+        return urlFinal + "/{" + id + "}";
+    }
 }
 
-const addSubTask = () => {
- 
+const createHttpGetRequest = (id = -1) => {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", createURL(id), true);
+}
+const createHttpPOSTRequest = (id = -1) => {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("POST", createURL(id), true);
+}
+const createHttpPUTRequest = (id, data) => {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open('PUT', createURL(id), true);
+    httpRequest.send(JSON.stringify(data));
+}
+const createHTTPDeleteRequest = (id) => {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open('DELETE', createURL(id), true);
+    httpRequest.send(null);
+}
+
+const addRootTask = () => {
+    createHttpPOSTRequest("POST");
+}
+
+const addSubTask = (id) => {
+    createHttpPOSTRequest("POST", id);
 }
 
 const getTasks = () => {
- 
+    createHttpGetRequest("GET");
 }
 
-const getTask = () => {
- 
+const getTask = (id) => {
+    createHttpGetRequest("GET", id);
 }
 
-const modifyTask = () => {
- 
+const modifyTask = (id, data) => {
+    createHttpPUTRequest(id, data);
 }
 
-const deleteTask = () => {
- 
+const deleteTask = (id) => {
+ createHTTPDeleteRequest(id);
 }
