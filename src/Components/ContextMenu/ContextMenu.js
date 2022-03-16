aab5a4525e15f4aea36933c857197fa294c37604
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-16 11:27:07 +0200

    Added Remove task functionality

--- src/Components/ContextMenu/ContextMenu.js
+++ src/Components/ContextMenu/ContextMenu.js
@@ -6,21 +6,24 @@ import './ContextMenu.css';
 class ContextMenu extends React.Component {
     render (){
         return <div class="context-button" onClick={this.toggleMenu}>
-            <div class="context-menu" id={"menu-".concat(this.props.level)}>
+            <div class="context-menu" id={"menu-".concat(this.props.level).concat(this.props.index)} on={this.mouseLeave}>
                 <button onClick={this.addTask}>Add subtask</button>
-                <button>Remove Task</button>
+                <button onClick={this.removeTask}>Remove Task</button>
             </div>
         </div>;
     }
 
     toggleMenu = () => {
-        const contextMenu = document.getElementById("menu-".concat(this.props.level));
+        const contextMenu = document.getElementById("menu-".concat(this.props.level).concat(this.props.index));
         if (contextMenu.style.visibility === "visible"){
             contextMenu.style.visibility = "hidden";
         } else {
             contextMenu.style.visibility = "visible";
         }
-    
+    }
+
+    mouseLeave = () => {
+        document.getElementById("menu-".concat(this.props.level)).visibility="hidden";
     }
 
     addTask = () => {
@@ -30,11 +33,23 @@ class ContextMenu extends React.Component {
         }
         const currentItemContainer = document.getElementById("item-container-level-".concat(this.props.level));
         //const parentTask= document.getElementById("parent-item-container-".concat(this.props.level));
-        const newTask = new Task({level :parseInt(this.props.level)+1});
+        //console.log(currentItemContainer.children.length);
+        const newTask = new Task({level :this.props.level+1, index: currentItemContainer.children.length});
         //ReactDOM.render(newTask.render(), currentItemContainer);
         ReactDOM.render(ReactDOM.createPortal(newTask.render(), currentItemContainer), document.createElement('div'));
-
-        
+    }
+    
+    removeTask = () =>{
+        //console.log("deleting".concat("task-container-".concat(this.props.level).concat(this.props.index)));
+        //ReactDOM.unmountComponentAtNode(document.getElementById("task-container-".concat(this.props.level).concat(this.props.index)));
+        //console.log(this.props.level);
+        //const itemContainer = document.getElementById("item-container-level-".concat(this.props.level-1));
+        //console.log(this.props.index);
+        //console.log(itemContainer.children[this.props.index])
+        //console.log(itemContainer.childNodes);
+        //console.log(document.getElementById("task-container-".concat(this.props.level).concat(this.props.index)))
+        document.getElementById("task-container-".concat(this.props.level).concat(this.props.index)).remove();
+        //itemContainer.removeChild(itemContainer.);
 
     }
 }
