292a968782731a39acf295c81d6fd38007f1106b
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-16 11:29:56 +0200

    Created Backend service shell

---
+++ src/Services/TaskHelper.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const addRootTask = () => {
+ 
+}
+
+const addSubTask = () => {
+ 
+}
+
+const getTasks = () => {
+ 
+}
+
+const getTask = () => {
+ 
+}
+
+const modifyTask = () => {
+ 
+}
+
+const deleteTask = () => {
+ 
+}
