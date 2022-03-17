df9a2870a705bdea03d753f8d51b1db8f49d9874
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-17 13:18:29 +0200

    Added class shell

---
+++ src/Components/EditComponent/EditComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class EditComponent extends React.Component {
+    render () {
+        return;
+    }
+}
+
+export default EditComponent;
---
+++ src/Components/RemindersComponent/RemindersComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class RemindersComponent extends React.Component {
+    render () {
+        return;
+    }
+}
+
+export default RemindersComponent;
