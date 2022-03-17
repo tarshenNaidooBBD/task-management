1f47f7a8dc8221ec4829102f25756e71166f715a
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-17 12:57:53 +0200

    Added default routing component
    Added default routing component (404 page not found)

---
+++ src/Components/RouteNotFoundComponent/RouteNotFoundComponnet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import react from 'react'
+
+class RouteNotFound extends React.Component {
+
+    render() {
+        return <div>Error 404 Page not found!</div>
+    }
+}
+
+export default RouteNotFound;
