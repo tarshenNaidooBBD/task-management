c3b8f08bcf734c6dfc0b95589283683c91f49ea4
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-18 15:17:02 +0200

    Reworked Context Menu
    Items are no longer deleted but hidden when tasks are removed. This resolves potential id duplication issues

--- src/Components/ContextMenuComponent/ContextMenuComponent.js
+++ src/Components/ContextMenuComponent/ContextMenuComponent.js
@@ -6,25 +6,26 @@ import './ContextMenuComponent.css';
 class ContextMenu extends React.Component {
     render (){
         return <div class="context-button" onClick={this.toggleMenu}>
-            <div class="context-menu" id={"menu-".concat(this.props.level).concat(this.props.index)} on={this.mouseLeave}>
+            <div class="context-menu" id={"menu-".concat(this.props.level).concat(this.props.index)} onMouseLeave={this.turnOffMenu}>
                 <button onClick={this.addTask}>Add subtask</button>
                 <button onClick={this.removeTask}>Remove Task</button>
             </div>
         </div>;
     }
 
+    turnOffMenu = () => {
+        const contextMenu = document.getElementById("menu-".concat(this.props.level).concat(this.props.index));
+        contextMenu.style.visibility = "hidden";
+    }
+
     toggleMenu = () => {
 
         const contextMenu = document.getElementById("menu-".concat(this.props.level).concat(this.props.index));
-        if (contextMenu.style.visibility === "visible"){
-            contextMenu.style.visibility = "hidden";
-        } else {
+        if (contextMenu.style.visibility === "hidden"){
             contextMenu.style.visibility = "visible";
+        } else {
+            contextMenu.style.visibility = "hidden";
         }
-    }
-
-    mouseLeave = () => {
-        document.getElementById("menu-".concat(this.props.level)).visibility="hidden";
     }
 
     addTask = () => {
@@ -49,7 +50,8 @@ class ContextMenu extends React.Component {
         //console.log(itemContainer.children[this.props.index])
         //console.log(itemContainer.childNodes);
         //console.log(document.getElementById("task-container-".concat(this.props.level).concat(this.props.index)))
-        document.getElementById("task-container-".concat(this.props.level).concat(this.props.index)).remove();
+        //document.getElementById("task-container-".concat(this.props.level).concat(this.props.index)).remove();
+        document.getElementById("task-container-".concat(this.props.level).concat(this.props.index)).style.display='none';
         //itemContainer.removeChild(itemContainer.);
 
     }
