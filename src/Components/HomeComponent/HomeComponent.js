0c2b738c54504df04fceecf38800c6820296c01b
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-18 15:15:52 +0200

    Added Tasks to home page
    Added TaskComponent functionality to HomeComponent

--- src/Components/HomeComponent/HomeComponent.js
+++ src/Components/HomeComponent/HomeComponent.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import HeaderComponent from '../HeaderComponent/HeaderComponent';
+import {getTasks} from '../../Services/TaskHelper'
+import TaskComponent from '../TaskComponent/TaskComponent';
+<script type="text/javascript" src='../../Services/TaskHelper'></script>
 
 class HomeComponent extends React.Component {
     render (){
-        return <HeaderComponent color="orange" titleText="Home Page"></HeaderComponent>
+        //getTasks();
+        return (
+            <div>
+                <HeaderComponent color="orange" titleText="Home"></HeaderComponent>
+                <TaskComponent level={0}index={0}></TaskComponent>
+            </div>
+        );
     }
 }
 
