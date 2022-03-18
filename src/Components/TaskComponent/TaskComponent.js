ba59eb826d4eedc1f5030786b1864d35ab1f8134
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-18 15:13:56 +0200

    Changed styling details

--- src/Components/TaskComponent/TaskComponent.js
+++ src/Components/TaskComponent/TaskComponent.js
@@ -2,19 +2,23 @@ import React from 'react';
 import './TaskComponent.css';
 import '../ContextMenuComponent/ContextMenuComponent';
 import ContextMenuComponent from '../ContextMenuComponent/ContextMenuComponent';
-import Task from '../../Objects/Task';
+import TaskItemComponent from '../TaskItemComponent/TaskItemComponent';
 
 class TaskComponent extends React.Component {
 
     render (){
         return <div class="task-container" id={"task-container-".concat(this.props.level).concat(this.props.index)}>
-            <span class="task-container-header">
-                <div class="task-container-title">Hey there</div>
-                <ContextMenuComponent level={this.props.level} index={this.props.index}></ContextMenuComponent>
-            </span>
-            <br></br>
-            <div class="item-container" id={"item-container-level-".concat(this.props.level)} children="0"></div>
-            
+            <div style={{'margin':10}}>
+                <span class="task-container-header">
+                    <div class="task-container-title">Hey there</div>
+                    <ContextMenuComponent level={this.props.level} index={this.props.index}></ContextMenuComponent>
+                </span>
+                <div style={{'margin-left':10}}>
+                    <br></br>
+                    <TaskItemComponent></TaskItemComponent>
+                    <div class="item-container" id={"item-container-level-".concat(this.props.level)}>Subtasks:</div>
+                </div>
+            </div>
         </div>
     }
 
