7a492ce1307495430686edd30e0fba5e75dd7857
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-03-18 15:13:32 +0200

    Created Task Item component

---
+++ src/Components/TaskItemComponent/TaskItemComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class TaskItemComponent extends React.Component {
+    render () {
+        return (
+            <div>
+                <p>ID</p>
+                <p>Title</p>
+                <p>Status</p>
+                <p>Duedate</p>
+            </div>
+        );
+    }
+}
+
+export default TaskItemComponent;
